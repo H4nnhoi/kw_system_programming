1ae37335d46de7ae69fb5dc6f328c6dd46fb1b39
hann <wjdgks1233@gmail.com>
2025-05-11 15:11:09 +0900

    feat : create connect_to_webserver, send_http_request, and receive_http_response methods

--- pj_2_3/serverUtils.c
+++ pj_2_3/serverUtils.c
@@ -1,5 +1,11 @@
-#include <stdio.h>
-#include <string.h>
+#include <stdio.h>        
+#include <stdlib.h>      
+#include <string.h>       
+#include <unistd.h>      
+#include <netdb.h>       
+#include <arpa/inet.h>    
+#include <netinet/in.h>   
+#include <sys/socket.h>   
 #define BUFFSIZE 1024
 
 char* get_parsing_url(char* request){
@@ -19,4 +25,65 @@ char* get_parsing_url(char* request){
         perror("method type wrong");
         exit(0);
     }
+}
+
+int connect_to_webserver(const char *hostname, int port) {
+    struct sockaddr_in server_addr;
+    struct hostent *host;
+    int sock;
+
+    if ((host = gethostbyname(hostname)) == NULL) {
+        perror("gethostbyname failed");
+        return -1;
+    }
+
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) return -1;
+
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port);
+    memcpy(&server_addr.sin_addr, host->h_addr, host->h_length);
+
+    if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        close(sock);
+        return -1;
+    }
+
+    return sock;
+}
+
+int send_http_request(int sockfd, const char* request) {
+    size_t total_sent = 0;
+    size_t request_len = strlen(request);
+    
+    while (total_sent < request_len) {
+        ssize_t sent = write(sockfd, request + total_sent, request_len - total_sent);
+        if (sent <= 0) {
+            perror("write to web server failed");
+            return -1;
+        }
+        total_sent += sent;
+    }
+    return 0;
+}
+
+
+int receive_http_response(int sockfd, char* buffer, size_t size) {
+    ssize_t total_received = 0;
+    ssize_t n;
+
+    while ((n = read(sockfd, buffer + total_received, size - total_received)) > 0) {
+        total_received += n;
+        if (total_received >= size) {
+            break;  // 버퍼 한계 도달
+        }
+    }
+
+    if (n < 0) {
+        perror("read from web server failed");
+        return -1;
+    }
+
+    return total_received;
 }
